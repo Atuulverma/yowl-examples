df78a4c50660e2b9f40dc93378534eced2f50958
Brian Brunner <brian.brunner@gmail.com>
2016-04-26 14:08:05 -0700

    Added classification example

---
+++ examples/classification/bot.js
@@ -0,0 +1,39 @@
+var yowl = require('yowl');
+var bot = yowl();
+
+bot.name = "Pizza Bot";
+
+var local = require('yowl-platform-cli');
+bot.extend(local);
+
+var classifier = require('yowl-classifier-bayes');
+var training = require('./training');
+bot.use(classifier({ training: training }));
+
+bot.use(function(context, event) {
+    return context.classification == "pizza_stats";
+  }, function(context, event, next) {
+    var message = "You have ordered 10 pizzas totaling $100.";
+    event.send(context, event, message, next.unroll);
+  });
+
+bot.use(function(context, event) {
+    return context.classification == "make_order";
+  }, function(context, event, next) {
+    var message = "Alright, I'll get to work making your pizza.";
+    event.send(context, event, message, next.unroll);
+  });
+
+bot.use(function(context, event) {
+    return context.classification == "order_status";
+  }, function(context, event, next) {
+    var message = "Your order is on it's way.";
+    event.send(context, event, message, next.unroll);
+  });
+
+bot.use(function(context, event, next) {
+    var message = "I'm sorry, I didn't understand your request. I only know things about pizza.";
+    event.send(context, event, message, next);
+  });
+
+bot.run();
